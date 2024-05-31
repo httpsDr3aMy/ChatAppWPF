f6a2ac5bdb1881f3a54958a15416e7e72dd9c5b1
Kamil <125757644+httpsDr3aMy@users.noreply.github.com>
2024-05-31 18:28:12 +0200

    chore: added in server console reading the username

--- ChatApp/Net/Server.cs
+++ ChatApp/Net/Server.cs
@@ -4,21 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
+using ChatApp.Net.IO;
 
 namespace ChatApp.Net
 {
     class Server
     {
         TcpClient _client;
+        PacketBuilder _packetBuilder;
         public Server()
         {
             _client = new TcpClient();
         }
-        public void ConnectToServer()
+        public void ConnectToServer(string username)
         {
             if(!_client.Connected)
             {
                 _client.Connect("127.0.0.1", 7000);
+                var connectPacket = new PacketBuilder();
+                connectPacket.WriteOPCode(0);
+                connectPacket.WriteString(username);
+                _client.Client.Send(connectPacket.GetPacketBytes());
             }
         }
     }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -6,13 +6,20 @@ namespace ChatServer
 {
     internal class Program
     {
+        static List<Client> _users;
         static TcpListener _listener;
         static void Main(string[] args)
         {
+            _users = new List<Client>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7000);
             _listener.Start();
 
-            var client = _listener.AcceptTcpClient();
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+
+            }
             Console.WriteLine("Client has connected!");
         }
     }
